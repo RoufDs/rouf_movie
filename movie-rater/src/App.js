ea3be2b0532af1af408e55dc768fb366c06451aa
RoufDs <four.3687zero@gmail.com>
2022-10-05 22:02:23 +0700

    useFetch

--- movie-rater/src/App.js
+++ movie-rater/src/App.js
@@ -6,13 +6,20 @@ import MovieForm from './components/MovieForm'
 import { useCookies } from 'react-cookie'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilm } from '@fortawesome/free-solid-svg-icons'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import {useFetch} from './hooks/useFetch'
 
 function App() {
 
   const [movies, setMovies] = useState([])
   const [selectdMovie, setSelectdMovie] = useState(null)
   const [editedMovie, setEditedMovie] = useState(null)
-  const [token] = useCookies(['mr-token'])
+  const [token, setToken, deleteToken] = useCookies(['mr-token'])
+  const [data, loading, error] = useFetch()
+
+  useEffect(() => {
+    
+  })
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/movies/", {
@@ -68,6 +75,10 @@ function App() {
     setMovies(newMovies)
   }
 
+  const logoutUser = () => {
+    deleteToken(['mr-token'])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -75,6 +86,7 @@ function App() {
           <FontAwesomeIcon icon={faFilm} />
           <span>Movie rater</span>
         </h1>
+        <FontAwesomeIcon icon={faSignOutAlt} onClick={logoutUser} />
       </header>
       <div className='layout'>
         <div>          
